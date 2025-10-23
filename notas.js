2c9fc5737da3dcb47e4ae72be998493743126ae0
Daniel Clovis da Costa <danielclovis233@gmail.com>
2025-10-22 23:54:11 -0300

    Add files via upload

--- notas.js
+++ notas.js
@@ -3,4 +3,6 @@ let segundoNumero = Number (prompt("Digite o segundo numero"))
 let terceiroNumero = Number (prompt("Digite o terceiro numero"))
 let quartoNumero = Number (prompt("Digite o quarto numero"))
 
-let 
+let media = (primeiroNumero + segundoNumero + terceiroNumero + quartoNumero) / 4
+
+alert(media)
