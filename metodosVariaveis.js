26aa5b745f26d3a6b7465551318f7213a6e63a31
Daniel Clovis da Costa <danielclovis233@gmail.com>
2025-10-30 21:15:03 -0300

    Add files via upload

---
+++ metodosVariaveis.js
@@ -0,0 +1,18 @@
+// to fixed (2) para nao ficar com muitas casas decimais 
+
+/* let nome = "Daniel"
+
+alert(nome.length) ele conta quantas letra dentro da variavel*/
+
+/* alert(nome.toUpperCase()) para deixar o nome em maiusculo */
+/* alert(nome.toLowerCase()) esse é para deixar tudo minusculo */
+
+
+/* alert(dinheiro.toLocaleString('pt-br' , {style: 'currency', currency: "BRL"}))
+*/ 
+
+/* let divisao = 2/3
+
+alert(divisao.toFixed(2))
+ */
+
